98eec55c4acbb52a8861eedf6bced3259abca972
RajNangare <>
2023-10-11 03:10:14 +0530

    updates

---
+++ script.js
@@ -0,0 +1,10 @@
+function fk(){
+    // let d = date();
+    // d += "She is Fked";
+    document.getElementById('demo').innerHTML = "Only your Mom is Fked LOL !";
+    all.style.color = "#dc143c";
+    demo.style.color = "#dc143c"
+    document.body.style.backgroundColor = "black";
+    value.style.backgroundColor = "#dc143c";
+    
+}
